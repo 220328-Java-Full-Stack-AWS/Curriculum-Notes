e4be9af145ac869f2fa4b310873ad7c8c64274f9
elmcgill <ethanlmcgill@gmail.com>
2022-04-05 09:36:42 -0500

    simple little tic tac toe game

--- demos/ticktacktoe/src/main/java/TicTacToeDriver.java
+++ demos/ticktacktoe/src/main/java/TicTacToeDriver.java
@@ -4,6 +4,7 @@ public class TicTacToeDriver {
 
     private Scanner scan = new Scanner(System.in);
     private int counter = 0;
+    private boolean tie;
 
     public static void main(String args[]){
 
@@ -15,11 +16,14 @@ public class TicTacToeDriver {
         char board[][] = {{'1','2','3'}, {'4','5','6'}, {'7', '8', '9'}};
         boolean isXTurn = true;
 
-
-
         //Test to see for the patterns
        while(!tttd.isGameOver(board)){
-           System.out.println("Choose a position on the current board");
+           if(isXTurn){
+               System.out.println("Player X choose a position");
+           }
+           else {
+               System.out.println("Player O choose a position");
+           }
            for(int i=0; i<board.length; i++){
                for(int j=0; j<board[i].length; j++){
                    System.out.print(board[i][j]);
@@ -28,7 +32,37 @@ public class TicTacToeDriver {
                System.out.println();
            }
 
-           String nextline = tttd.scan.nextLine();
+           int position = tttd.scan.nextInt();
+
+           int row = ((position+2)/3)-1;
+           int col = (position+2) % 3;
+
+           if(board[row][col] != 'O' && board[row][col] != 'X'){
+               if(isXTurn){
+                   board[row][col] = 'X';
+               }
+               else {
+                   board[row][col] = 'O';
+               }
+           }
+
+           tttd.counter++;
+
+           if(tttd.isGameOver(board)){
+               if(isXTurn && !tttd.tie){
+                   System.out.println("Player X has won the game");
+               }
+               else if(!isXTurn && !tttd.tie){
+                   System.out.println("Player O has won the game");
+               }
+               else{
+                   System.out.println("There was a tie");
+               }
+           }
+
+           //System.out.println(tttd.counter);
+
+           isXTurn = !isXTurn;
 
        }
 
@@ -38,10 +72,9 @@ public class TicTacToeDriver {
     }
 
     public boolean isGameOver(char[][] board){
-        counter++;
-        //O,O,O
-        //-,-,-
-        //-,-,-
+        //O,-,-
+        //O,-,-
+        //O,-,-
         if(board[0][0] == board[0][1] && board[0][1] == board[0][2]){
             return true;
         }
@@ -51,7 +84,23 @@ public class TicTacToeDriver {
         else if(board[2][0] == board[2][1] && board[2][1] == board[2][2]){
             return true;
         }
+        else if(board[0][0] == board[1][0] && board[1][0] == board[2][0]){
+            return true;
+        }
+        else if(board[0][1] == board[1][1] && board[1][1] == board[2][1]){
+            return true;
+        }
+        else if(board[0][2] == board[1][2] && board[1][2] == board[2][2]){
+            return true;
+        }
+        else if(board[0][0] == board[1][1] && board[1][1] == board[2][2]){
+            return true;
+        }
+        else if(board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+            return true;
+        }
         else if(counter == 9){
+            tie = true;
             return true;
         }
         else{
