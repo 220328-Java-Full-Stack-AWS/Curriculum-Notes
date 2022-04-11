68d983f5213317f1a447206f38942262137eeeb9
Kyle <Kyle.Plummer@revature.com>
2022-04-11 14:44:27 -0500

    Revagenda JDBC Connection

---
+++ demos/Revagenda/src/main/java/revagenda/Main.java
@@ -0,0 +1,53 @@
+package revagenda;
+
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            Connection conn = ConnectionManager.getConnection();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
+        /*
+        Need user input - console input via scanner
+            later replaced with GUI - web interface in a browser
+            HTTP request from client to server
+
+        Output - console output
+            later replaced with web GUI
+
+        Validation - validate user input
+
+        Data Persistence - store our info (to-do items) in a relational database - postgreSQL
+                            store users - auth purposes
+
+        client-server communication - our client should be able to send data and receive HTTP requests
+        We need an API and we need to have our client "consume" the API
+
+        Auth - We will want to allow users to register and login to their account. We want
+                to-do items to be associated with particular users.
+
+        Business logic - catch-all for everything not specified elsewhere.
+
+        Hosting - we want to put our server in the cloud, we want our database also in the cloud, and we want to
+        serve up our web GUI from some remotely accessible location.
+
+        Translating from java memory to SQL schema. We will start with plain JDBC code (execute SQL scripts
+        from java app), later we will put an ORM in place, which is a powerful tool that does this for us.
+
+        Accessibility - make site user friendly.
+
+        Documentation - javadocs, other inline documentation, as well as overview documentation, design docs like
+                        ERD, UML models
+
+
+         */
